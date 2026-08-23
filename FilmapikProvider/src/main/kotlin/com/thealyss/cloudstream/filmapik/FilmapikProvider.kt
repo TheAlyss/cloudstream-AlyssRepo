46031990f2341af3b7dd9f1dba56f20440bbaa91
TheAlyss <thealyss@users.noreply.github.com>
2026-08-23 23:39:12 +0800

    Import LoadResponse.Companion.addTrailer in FilmapikProvider

--- FilmapikProvider/src/main/kotlin/com/thealyss/cloudstream/filmapik/FilmapikProvider.kt
+++ FilmapikProvider/src/main/kotlin/com/thealyss/cloudstream/filmapik/FilmapikProvider.kt
@@ -2,6 +2,7 @@ package com.thealyss.cloudstream.filmapik
 
 import android.util.Log
 import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.utils.*
 import org.jsoup.nodes.Element
 import org.json.JSONArray
